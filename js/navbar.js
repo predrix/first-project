cc5115ededef8f286b039df6e4016466c0e27095
unknown <predrix@gmail.com>
2020-01-28 13:01:06 +0100

    final

--- js/navbar.js
+++ js/navbar.js
@@ -3,14 +3,15 @@
 		if (($(this).scrollTop() > $('header').height()) && ($(document).width() > 576)) {
 			$('nav').addClass('fixed-top');
 			$('.navigation').css({background: '#F38181', opacity: '0.8'});
-			$('body').css('padding-top', $('.navigation').height() + 'px');
-		} else if(($(this).scrollTop() < $('header').height()) && ($(document).width() > 576)) {
+			$('body').css('padding-top', $('nav').height() + 'px');
+		} else if(($(this).scrollTop() < $('header').height()) && ($(this).scrollTop()) <= 0 && ($(document).width() > 576)) {
 			$('nav').removeClass('fixed-top');
 			$('.navigation').css({background: 'transparent'});
-			$('body').css('padding-top', 0);
-		};
+			$('body').css('padding-top', '0');
+		} 
 });
 
+console.log($('nav').height());
 function scroll(e) {
 
     let href = $(this).attr('href');
