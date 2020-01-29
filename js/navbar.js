957201e274239115c5a278a4eebc90d3423e20d3
unknown <predrix@gmail.com>
2020-01-29 06:45:39 +0100

    final

--- js/navbar.js
+++ js/navbar.js
@@ -4,11 +4,18 @@
 			$('nav').addClass('fixed-top');
 			$('.navigation').css({background: '#F38181', opacity: '0.8'});
 			$('body').css('padding-top', $('nav').height() + 'px');
-		} else if(($(this).scrollTop() < $('header').height()) && ($(this).scrollTop()) <= 0 && ($(document).width() > 576)) {
+		} else if((($(this).scrollTop() < $('header').height()) || ($(this).scrollTop()) === 0) && ($(document).width() > 576)) {
 			$('nav').removeClass('fixed-top');
 			$('.navigation').css({background: 'transparent'});
 			$('body').css('padding-top', '0');
 		} 
+});
+$(window).resize(function() {
+	if ($(this).scrollTop() === 0) {
+		$('nav').removeClass('fixed-top');
+		$('.navigation').css({background: 'transparent'});
+		$('body').css('padding-top', '0');
+	}
 });
 
 console.log($('nav').height());
